059cad99fc8334ec2a9ee094c6efdccd46dee938
Mario Camacho Prieto <mario.claserebeca@gmail.com>
2025-08-27 01:01:04 +0200

    Create ejercicio7.js

---
+++ ejercicio7.js
@@ -0,0 +1,26 @@
+const tracks = [
+  { title: 'Enter Sandman', genre: 'Metal' },
+  { title: 'Back in Black', genre: 'Rock' },
+  { title: 'Bohemian Rhapsody', genre: 'Rock' },
+  { title: 'Blinding Lights', genre: 'Pop' },
+  { title: 'Old Town Road', genre: 'Country' },
+  { title: 'Smells Like Teen Spirit', genre: 'Grunge' },
+  { title: 'Bad Guy', genre: 'Pop' },
+  { title: 'Thunderstruck', genre: 'Rock' },
+  { title: 'Hotel California', genre: 'Rock' },
+  { title: 'Stairway to Heaven', genre: 'Rock' }
+];
+
+const categorizedTracks = {};
+
+for (let i = 0; i < tracks.length; i++) {
+  const track = tracks[i];
+  const genre = track.genre;
+
+  if (!categorizedTracks[genre]) {
+    categorizedTracks[genre] = [];
+  }
+  categorizedTracks[genre].push(track);
+}
+
+console.log(categorizedTracks);
